3a15a48be54f0a70a0d03a413c1e34a06a918ac0
김영준 <yjkim@selab.co.kr>
2021-12-13 15:41:15 +0900

    [CNF] 컨테이너 active profile설정, 타임존 설정

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,10 @@
-#mvn com.google.cloud.tools:jib-maven-plugin:dockerBuild -Dimage=springbootblueprints
-
 docker build . -t springbootblueprints
 
 docker rm -f springbootblueprints
 
 docker run -d --restart unless-stopped \
+  -e SPRING_PROFILES_ACTIVE=staging \
+  -e TZ=Asia/Seoul \
   --name springbootblueprints -p 8000:8000 \
   -v /docker_data/springbootblueprints:/springbootblueprints \
   springbootblueprints
