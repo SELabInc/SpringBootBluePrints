c1dbb0bc5a9547f9774fdb7161768f94bea6d94c
정보현 <bhjung@selab.co.kr>
2021-12-15 14:33:45 +0900

    [FEA] 시보 로그 추가
    TaskScheduler 예시로 매 시간 로그찍는 기능 추가
    #CFD-14 work today 1h

---
+++ src/main/java/com/selab/springbootblueprints/config/TaskSchedulerConfig.java
@@ -0,0 +1,38 @@
+package com.selab.springbootblueprints.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Configuration
+public class TaskSchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setDaemon(true);
+        threadPoolTaskScheduler.setPoolSize(3);
+        threadPoolTaskScheduler.setThreadNamePrefix("task-scheduler-");
+        threadPoolTaskScheduler.initialize();
+
+        setSampleTask(threadPoolTaskScheduler);
+
+        return threadPoolTaskScheduler;
+    }
+
+    private void setSampleTask(TaskScheduler scheduler) {
+        // 'sec min hour dayOfMonth month dayOfWeek (year / optional)'
+        String cronPattern = "0 0 * * * ?";
+
+        scheduler.schedule(() -> {
+            String logMessage = String.format("Task schedule sample time-signal log: %s o'clock", LocalDateTime.now().getHour());
+            log.trace(logMessage);
+        }, new CronTrigger(cronPattern));
+    }
+}
