1e533a9860a65b90ab92d2d345d6c412450298f9
박예빈 <codepark.kr@gmail.com>
2021-12-15 13:59:37 +0900

    [FEA] SwaggerConfig 수정 및 기본 설정
    - SwaggerConfig 파일의 기본 설정 구문 추가
    - 해당 페이지 title, description, version 정보 기술
    - basePackage 설정하여 현재 사용 중인 api 리스트업

--- src/main/java/com/selab/springbootblueprints/config/SwaggerConfig.java
+++ src/main/java/com/selab/springbootblueprints/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.selab.springbootblueprints.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +14,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.ant("/api/**"))
+//                .build();
+//    }
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/api/**"))
-                .build();
+    public Docket SwaggerConfig(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.selab.springbootblueprints")).paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("SELAB-DEV : SPRING BOOT BLUEPRINTS APIs").version("1.0.0")
+                .description("SWAGGER_2 :: SELAB-DEV SPRING BOOT BLUEPRINTS APIs LIST").build();
     }
 }
 
