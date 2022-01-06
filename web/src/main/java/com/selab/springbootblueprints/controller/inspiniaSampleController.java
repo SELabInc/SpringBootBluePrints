949ba14b6059e690b82bdbcf73466a73713abb40
hajinryu <jinnyryu51@gmail.com>
2022-01-06 14:54:47 +0900

    [REF] network.area 설정에 따라 라이브러리를 내부저장 버전 CDN 버전으로 선택가능하도록 추가함
    - application.properties 파일의 network.area 변수를 lan 또는 wan 으로 설정
    - 컨트롤러에서 model에 resourceType을 넣고 view return
    
    #CFD-62 Stage Develop

--- web/src/main/java/com/selab/springbootblueprints/controller/inspiniaSampleController.java
+++ web/src/main/java/com/selab/springbootblueprints/controller/inspiniaSampleController.java
@@ -1,7 +1,9 @@
 package com.selab.springbootblueprints.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -9,21 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/inspinia")
 public class inspiniaSampleController {
 
-    @RequestMapping("/samplePage1")
-    public String getSample1() {
+    @Value("${network.area}")
+    private String resourceType;
 
+    @RequestMapping("/samplePage1")
+    public String getSample1(Model model) {
+
+        model.addAttribute("resourceType",resourceType);
         return "inspinia/samplePage1";
     }
 
     @RequestMapping("/samplePage2")
-    public String getSample2() {
+    public String getSample2(Model model) {
 
+        model.addAttribute("resourceType",resourceType);
         return "inspinia/samplePage2";
     }
 
-    @RequestMapping("/samplePage3")
-    public String getSample3() {
-
-        return "inspinia/samplePage3";
-    }
 }
