5b662e9aa1fd9dbe521587b977f25d18b572a3dc
정보현 <bhjung@selab.co.kr>
2022-01-06 13:14:51 +0900

    [REF] P6spy 설정 분리
    p6spy모듈로 기존 p6spy 관련 소스 이동
    lib/pom.xml
     - lombok 추가
    
    #CFD-68 state test
    #CFD-68 work today 1h

--- p6spy/src/main/java/com/selab/springbootblueprints/p6spy/P6spySqlFormatConfig.java
+++ p6spy/src/main/java/com/selab/springbootblueprints/p6spy/P6spySqlFormatConfig.java
@@ -1,15 +1,20 @@
-package com.selab.springbootblueprints.config;
+package com.selab.springbootblueprints.p6spy;
 
 import com.p6spy.engine.logging.Category;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
-import org.hibernate.engine.jdbc.internal.BasicFormatterImpl;
 import org.hibernate.engine.jdbc.internal.FormatStyle;
+import org.hibernate.engine.jdbc.internal.Formatter;
 
 
 public class P6spySqlFormatConfig implements MessageFormattingStrategy {
 
     private final String STATEMENT_SQL_MESSAGE_FORMAT = "#%s | url: %s | connectionID: %s | category: %s | elapsed: %sms\n\t%s\n";
     private final String EITHER_CATEGORY_MESSAGE_FORMAT = "#%s | url: %s | connectionID: %s | category: %s | prepared: %s | sql: %s | elapsed: %sms";
+    private final Formatter formatter;
+
+    public P6spySqlFormatConfig() {
+        this.formatter = FormatStyle.BASIC.getFormatter();
+    }
 
     @Override
     public String formatMessage(final int connectionId, final String now, final long elapsed, final String category, final String prepared, String sql, final String url) {
@@ -29,15 +34,7 @@ public class P6spySqlFormatConfig implements MessageFormattingStrategy {
 
         String result = "\n\tquery is empty";
         if (!trimLowCaseSql.isBlank()) {
-            if (trimLowCaseSql.startsWith("create") || trimLowCaseSql.startsWith("alter") || trimLowCaseSql.startsWith("comment")) {
-                result = FormatStyle.DDL
-                        .getFormatter()
-                        .format(sql);
-            } else {
-                result = FormatStyle.BASIC
-                        .getFormatter()
-                        .format(sql);
-            }
+            this.formatter.format(sql);
         }
 
         return result;
