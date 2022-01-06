c894e00458e9c1dca9fca5eb5a7db1fc16413e3b
정보현 <bhjung@selab.co.kr>
2022-01-06 13:14:51 +0900

    [REF] P6spy 설정 분리
    p6spy모듈로 기존 p6spy 관련 소스 이동
    lib/pom.xml
     - lombok 추가
    
    #CFD-68 state test
    #CFD-68 work today 1h

--- p6spy/src/main/java/com/selab/springbootblueprints/config/P6spyLogMessageFormatConfig.java
+++ p6spy/src/main/java/com/selab/springbootblueprints/config/P6spyLogMessageFormatConfig.java
@@ -1,12 +1,21 @@
 package com.selab.springbootblueprints.config;
 
 import com.p6spy.engine.spy.P6SpyOptions;
+import com.selab.springbootblueprints.p6spy.CustomQueryFormatter;
+import com.selab.springbootblueprints.p6spy.P6spySqlFormatConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+
 @Configuration
 public class P6spyLogMessageFormatConfig {
+
+    @Bean
+    public CustomQueryFormatter customQueryFormatter() {
+        return CustomQueryFormatter.builder().build();
+    }
 
     @PostConstruct
     public void setLogMessageFormat() {
