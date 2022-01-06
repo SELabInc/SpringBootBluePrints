dd421ea9cceca1de442290f14f3a07e52b6707fe
정보현 <bhjung@selab.co.kr>
2022-01-06 19:21:20 +0900

    [FEA] 커스텀 포맷 구현
    CustomMultiLineFormatter
     - 기존에 FormatStyle.BASIC의 Fomatter의 소스를 긁어와서 내부에 Select일 시 줄 안바꾸는 로직 추가
     - 코드 개선 필요 로직 흐름이 매우 눈에 안들어오는 형태
     - 사용하면서 테스트 필요
    
    CustomMessageFormattingStrategy
     - 클래스명 리펙터링
     - 멀티라인으로 파싱 후 컬러 하이라이팅 추가하도록 추가
     - String Tokenizer를 두 번 타는 형태, 이후 CustomMultiLineFormatter에 하이라이트 기능을 녹여서 한 포맷터만 타도록 개선 필요 (현재는 두 포매터의 흐름이 상이하여 결합 공수 과다로 홀드)
    
     #CFD-55 work today 3h
     #CFD-55 state test

--- p6spy/src/main/java/com/selab/springbootblueprints/config/P6spyLogMessageFormatConfig.java
+++ p6spy/src/main/java/com/selab/springbootblueprints/config/P6spyLogMessageFormatConfig.java
@@ -1,9 +1,7 @@
 package com.selab.springbootblueprints.config;
 
 import com.p6spy.engine.spy.P6SpyOptions;
-import com.selab.springbootblueprints.p6spy.CustomQueryFormatter;
-import com.selab.springbootblueprints.p6spy.P6spySqlFormatConfig;
-import org.springframework.context.annotation.Bean;
+import com.selab.springbootblueprints.p6spy.CustomMessageFormattingStrategy;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -12,13 +10,8 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class P6spyLogMessageFormatConfig {
 
-    @Bean
-    public CustomQueryFormatter customQueryFormatter() {
-        return CustomQueryFormatter.builder().build();
-    }
-
     @PostConstruct
     public void setLogMessageFormat() {
-        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfig.class.getName());
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(CustomMessageFormattingStrategy.class.getName());
     }
 }
--- p6spy/src/main/java/com/selab/springbootblueprints/p6spy/CustomMessageFormattingStrategy.java
+++ p6spy/src/main/java/com/selab/springbootblueprints/p6spy/CustomMessageFormattingStrategy.java
@@ -6,15 +6,10 @@ import org.hibernate.engine.jdbc.internal.FormatStyle;
 import org.hibernate.engine.jdbc.internal.Formatter;
 
 
-public class P6spySqlFormatConfig implements MessageFormattingStrategy {
+public class CustomMessageFormattingStrategy implements MessageFormattingStrategy {
 
     private final String STATEMENT_SQL_MESSAGE_FORMAT = "#%s | url: %s | connectionID: %s | category: %s | elapsed: %sms\n\t%s\n";
     private final String EITHER_CATEGORY_MESSAGE_FORMAT = "#%s | url: %s | connectionID: %s | category: %s | prepared: %s | sql: %s | elapsed: %sms";
-    private final Formatter formatter;
-
-    public P6spySqlFormatConfig() {
-        this.formatter = FormatStyle.BASIC.getFormatter();
-    }
 
     @Override
     public String formatMessage(final int connectionId, final String now, final long elapsed, final String category, final String prepared, String sql, final String url) {
@@ -34,7 +29,12 @@ public class P6spySqlFormatConfig implements MessageFormattingStrategy {
 
         String result = "\n\tquery is empty";
         if (!trimLowCaseSql.isBlank()) {
-            this.formatter.format(sql);
+            result = sql;
+            result =  new CustomMultiLineFormatter()
+                    .format(result);
+            result = FormatStyle.HIGHLIGHT
+                    .getFormatter()
+                    .format(result);
         }
 
         return result;
