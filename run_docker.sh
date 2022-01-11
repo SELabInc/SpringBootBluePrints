47a93e06cf8e9994f5b6ff79ece7b265bc54e52a
정보현 <bhjung@selab.co.kr>
2022-01-11 18:02:49 +0900

    [BUG] run docker fail
    properties 구조 후 스크립트 파일에 해당 부분 미적용하여 해당 부분 수정
    ([CNF] properties 분리 (aedd3a52) 참고)

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@ docker build . -t springbootblueprints
 docker rm -f springbootblueprints
 
 docker run -d --restart unless-stopped \
-  -e SPRING_PROFILES_ACTIVE=h2 \
+  -e SPRING_PROFILES_ACTIVE=dev \
   -e TZ=Asia/Seoul \
   --name springbootblueprints -p 11000:11000 \
   -v /docker_data/springbootblueprints:/springbootblueprints \
